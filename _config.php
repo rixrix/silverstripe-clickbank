530b121e193bf17f31a86171c33167a36ff3088e
rixrix <rsentino@g2ix.com>
2011-06-18 01:30:13 +1200

    cosmetics

--- _config.php
+++ _config.php
@@ -3,7 +3,7 @@
  * ClickBank config file
  * 
  * @package		silverstripe-clickbank
- * @author 		Richard Sentino <rix@mindginative.com>
+ * @author		Richard Sentino <rix@mindginative.com>
  */
 
 // Intercept ClickBank request and forward all request to ClickBank_Controller
