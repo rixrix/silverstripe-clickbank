256f72b198fd0aec1b756549e2ef2f642d809c05
rixrix <rsentino@g2ix.com>
2011-06-18 20:01:52 +1200

    Added support for i18n

---
+++ lang/en_US.php
@@ -0,0 +1,79 @@
+<?php
+
+global $lang;
+
+// Messages
+$lang['en_US']['ClickBank']['MESSAGE_WARNINGMISSINGMODULE'] = '<p class="message warning">Warning: The ClickBank module requires the Member Profiles and Orderable module.</p>';
+
+// Config
+$lang['en_US']['ClickBank']['CONFIG_GENERAL_SETTINGS_SECTION_TITLE'] = '<h2>General Settings</h2>';
+$lang['en_US']['ClickBank']['CONFIG_CLICKBANKID'] = 'ClickBank ID';
+$lang['en_US']['ClickBank']['CONFIG_SECRETKEY'] = 'Secret Key';
+$lang['en_US']['ClickBank']['CONFIG_SKINID'] = 'ClickBank Skin ID';
+$lang['en_US']['ClickBank']['CONFIG_IPN_URL'] = 'IPN URL';
+$lang['en_US']['ClickBank']['CONFIG_IPN_URL_LABEL'] = 'Copy/Paste the IPN URL into your ClickBank IPN configuration';
+$lang['en_US']['ClickBank']['CONFIG_API_SECTION_TITLE'] = '<h2>API Settings</h2>';
+$lang['en_US']['ClickBank']['CONFIG_API_KEY'] = 'API Key';
+$lang['en_US']['ClickBank']['CONFIG_API_DEVELOPER_KEY'] = 'Developer API Key';
+$lang['en_US']['ClickBank']['CONFIG_SITE_MODE_SECTION_TITLE'] = '<h2>Site Mode</h2>';
+$lang['en_US']['ClickBank']['CONFIG_SITE_MODE'] = 'Test mode ?';
+
+// Profile
+$lang['en_US']['ClickBank']['PROFILE_ACCOUNT_TYPE_SECTION_TITLE'] = 'Account Type';
+$lang['en_US']['ClickBank']['PROFILE_ACCOUNT_TYPE'] = 'ClickBank Account Type';
+$lang['en_US']['ClickBank']['PROFILE_DETAILS_SECTION_TITLE'] = 'Profile Details';
+$lang['en_US']['ClickBank']['PROFILE_ADDRESS_1'] = 'Address 1';
+$lang['en_US']['ClickBank']['PROFILE_ADDRESS_2'] = 'Address 2';
+$lang['en_US']['ClickBank']['PROFILE_CITY'] = 'City';
+$lang['en_US']['ClickBank']['PROFILE_ZIP'] = 'Zip Code';
+$lang['en_US']['ClickBank']['PROFILE_COUNTY'] = 'County';
+$lang['en_US']['ClickBank']['PROFILE_STATE'] = 'State';
+$lang['en_US']['ClickBank']['PROFILE_COUNTRY'] = 'Country';
+$lang['en_US']['ClickBank']['PROFILE_SHIPPING_STATE'] = 'Shipping State';
+$lang['en_US']['ClickBank']['PROFILE_SHIPPING_ZIP'] = 'Shipping Zip Code';
+$lang['en_US']['ClickBank']['PROFILE_SHIPPING_COUNTRY'] = 'Shipping Country';
+
+// After Purchase Page (APP)
+$lang['en_US']['ClickBank']['APP_REPLACEMENT_LABEL'] = 'Replacements: $CBReceipt = ClickBank Receipt, $CBName = ClickBank Member Name';
+$lang['en_US']['ClickBank']['APP_VALIDATE_REQUEST'] = 'Validate ClickBank Request ?';
+$lang['en_US']['ClickBank']['APP_LOGIN_AFTER_VALIDATION'] = 'Login member after ClickBank Validation ?';
+$lang['en_US']['ClickBank']['APP_LOGIN_AFTER_VALIDATION_LABEL'] = "NOTE: You need to tick 'Validate ClickBank Request' if you want to login the user after validation";
+
+// Product Page (PP)
+$lang['en_US']['ClickBank']['PP_SETTINGS_SECTION_TITLE'] = '<h2>ClickBank Details</h2>';
+$lang['en_US']['ClickBank']['PP_REDIRECT_TO_CLICKBANK'] = 'Redirect To ClickBank';
+$lang['en_US']['ClickBank']['PP_CLICKBANK_PRODUCT_ID'] = 'ClickBank Product ID';
+$lang['en_US']['ClickBank']['PP_PRODUCT_URL_ALIAS'] = 'Product URL Alias';
+$lang['en_US']['ClickBank']['PP_CLICKBANK_PRODUCT_URL_ALIAS'] = 'ClickBank Product URL';
+
+// IPN Log (IL)
+$lang['en_US']['ClickBank']['IL_CPRODITEM'] = 'Item #';
+$lang['en_US']['ClickBank']['IL_CPRODTITLE'] = 'Product Name';
+$lang['en_US']['ClickBank']['IL_CPRODTYPE'] = 'Type';
+$lang['en_US']['ClickBank']['IL_CTRANSAFFILIATE'] = 'Affiliate';
+$lang['en_US']['ClickBank']['IL_CACCOUNTMOUNT'] = 'Account Amount';
+$lang['en_US']['ClickBank']['IL_CORDERAMOUNT'] = 'Order Amount';
+$lang['en_US']['ClickBank']['IL_CTRANSPAYMENTMETHOD'] = 'Payment Method';
+$lang['en_US']['ClickBank']['IL_CCURRENCY'] = 'Currency';
+$lang['en_US']['ClickBank']['IL_CTRANSPUBLISHER'] = 'Transaction Publisher';
+$lang['en_US']['ClickBank']['IL_CTRANSROLE'] = 'Transaction Role';
+$lang['en_US']['ClickBank']['IL_CRECEIPT'] = 'Receipt';
+$lang['en_US']['ClickBank']['IL_CTID'] = 'Transaction ID';
+$lang['en_US']['ClickBank']['IL_CTRANSTIME'] = 'Date of Transaction';
+$lang['en_US']['ClickBank']['IL_CVENDTHRU'] = 'Extra Information';
+$lang['en_US']['ClickBank']['IL_CUPSELLRECEIPT'] = 'Upsell Receipt';
+$lang['en_US']['ClickBank']['IL_CTRANSACTION'] = 'Transaction Type';
+$lang['en_US']['ClickBank']['IL_CREBILLAMNT'] = 'Rebill Amount';
+$lang['en_US']['ClickBank']['IL_CPROCESSEDPAYMENTS'] = 'Processed Payments';
+$lang['en_US']['ClickBank']['IL_CFUTUREPAYMENTS'] = 'Future Payments';
+$lang['en_US']['ClickBank']['IL_CNEXTPAYMENTDATE'] = 'Next Payment Date';
+$lang['en_US']['ClickBank']['IL_CREBILLSTATUS'] = 'Rebill Status';
+
+// Text
+$lang['en_US']['ClickBank']['ADDRESS_HEADER'] = 'Address';
+$lang['en_US']['ClickBank']['SHIPPING_ADDRESS_HEADER'] = 'Shipping Address';
+
+// Button
+$lang['en_US']['ClickBank']['RETURN_MAIN_BUTTON'] = 'Return to main';
+
+?>
