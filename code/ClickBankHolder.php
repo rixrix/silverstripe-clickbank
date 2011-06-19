47227613c0416474ff2660036f8022349071786d
rixrix <rsentino@g2ix.com>
2011-06-19 23:54:35 +1200

    added product pagination list display

--- code/ClickBankHolder.php
+++ code/ClickBankHolder.php
@@ -7,10 +7,44 @@
  * @author 		Richard Sentino <rix@mindginative.com>
  */
 class ClickBankHolder extends Page {
+	static $db = array (
+		'ItemsPerPage' => 'Int'
+	);
+	
+	static $defaults = array(
+		'ItemsPerPage' => 10
+	);
+	
 	static $allowed_children = array (
 		'ClickBankProduct'
 	);
+	
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab('Root.Content.Main', new TextField('ItemsPerPage', 'Items Per Page'));
+		
+		return $fields;
+	}
 }
 
 class ClickBankHolder_Controller extends Page_Controller {
+	
+	/**
+	 * Displays product list
+	 * 
+	 * @param	none
+	 * @return	Object	result set
+	 */
+	public function ClickBankProducts() {
+		if(!isset($_GET['start']) || !is_numeric($_GET['start']) || (int)$_GET['start'] < 1) { 
+			$_GET['start'] = 0; 
+		}
+		$start = $_GET['start'];
+		
+		if($products = DataObject::get('ClickBankProduct', "`ParentID` = '{$this->ID}'", 'Created DESC', '', "{$start}, {$this->ItemsPerPage}")) {
+			return $products;
+		}
+
+		return false;
+	}
 }
